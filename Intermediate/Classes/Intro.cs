d26e9e8b3ddfd7a3062d8a297dd40626423aa8e3
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-02-16 22:44:49 -0800

    deep dive into classes

---
+++ Intermediate/Classes/Intro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Intermediate.Classes
+{
+    public class Intro
+    {
+        public string Name = "Kingsley";
+
+        public void GetName()
+        {
+            Console.WriteLine(Name);
+        }
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, My Name is {1}", to, Name);
+        }
+    }
+}
+
---
+++ Intermediate/Program.cs
@@ -0,0 +1,17 @@
+﻿using Intermediate.Classes;
+using Intermediate;
+
+namespace Intermediate;
+
+class Entry
+{
+
+    static void Main(string[] args)
+    {
+        var introduce = new Intro();
+
+        introduce.GetName();
+        introduce.Introduce("Mosh");
+    }
+}
+

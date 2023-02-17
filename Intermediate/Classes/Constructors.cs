433eae7997e3b0ef777996721557cb5b2a47da6c
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-02-16 22:53:14 -0800

    static and instance classes and constructors

---
+++ Intermediate/Classes/Constructors.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Intermediate.Classes
+{
+	public class Customer
+	{
+
+        public String Name;
+        public int Id;
+
+		public Customer()
+		{
+            //calling a constructor without an initialization
+		}
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        public Customer(string name, int id)
+        {
+            this.Name = name;
+            this.Id = id;
+        }
+    }
+}
+
--- Intermediate/Program.cs
+++ Intermediate/Program.cs
@@ -5,13 +5,17 @@ namespace Intermediate;
 
 class Entry
 {
-
     static void Main(string[] args)
     {
         var introduce = new Intro();
 
         introduce.GetName();
         introduce.Introduce("Mosh");
+
+
+        var customer = new Customer("Merit", 1);
+
+        Console.WriteLine("Our customer name is {0} and his id is ID-0{1}", customer.Name, customer.Id);
     }
 }
 

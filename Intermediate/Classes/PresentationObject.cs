909c1ab57ca663bc2d845c1ac787fa1945af9e4c
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-02-18 08:07:36 -0800

    Inheritance Init

---
+++ Intermediate/Classes/PresentationObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Intermediate.Classes
+{
+	public class PresentationObject
+	{
+
+		public int Width { get; set; }
+		public int Height { get; set; }
+
+
+		public void Copy()
+		{
+			Console.WriteLine("Object was with height {0} and width {1} was coppied to clipboard", this.Height, this.Width);
+		}
+
+        public void Duplicate()
+        {
+			Console.WriteLine("Object was duplicated");
+        }
+
+
+    }
+}
+
---
+++ Intermediate/Classes/Text.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Intermediate.Classes
+{
+	public class Text : PresentationObject
+	{
+		public int FontSize { get; set; }
+		public int FontFamily { get; set; }
+
+		public void AddHyperLink(string url)
+		{
+			Console.WriteLine("HyperLink was Added");
+		}
+
+	}
+}
+

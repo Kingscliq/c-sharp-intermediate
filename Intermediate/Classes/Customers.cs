cd834822ca5b85ae27c4c06e6b833e3ba79c1733
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-02-19 07:03:31 -0800

    add: customers orders

---
+++ Intermediate/Classes/Customers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Intermediate.Classes
+{
+	public class Customers
+	{
+		public int Id { get; set;}
+		public string? Name { get; set; }
+
+
+		public void GetCustomerRating()
+		{
+		   var rating = this.CalculateRating();
+
+		   if(rating == 0)
+			{
+				Console.WriteLine("Customer Should not be promoted");
+			}
+			else
+			{
+				Console.WriteLine("Customers Should be Promoted");
+			}
+		}
+
+		private int CalculateRating()
+		{
+			return 0;
+		}
+	}
+}
+
